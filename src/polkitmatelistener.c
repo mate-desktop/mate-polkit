35aaa5a7175c482e09c31e575bdeb33b1a12c822
raveit65 <mate@raveit.de>
2021-06-22 13:59:30 +0200

    update copyright to 2021

--- src/polkitmatelistener.c
+++ src/polkitmatelistener.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009 Red Hat, Inc.
+ * Copyright (C) 2012-2021 MATE Developers
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
