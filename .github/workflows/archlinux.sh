1e3b7ea21105ec20f88dd8a01ad9387ce4c7dba5
mbkma <johannes.unruh@fau.de>
2026-01-28 08:50:07 +0100

    ci: add missing packages and downstream links to CI scripts

--- .github/workflows/archlinux.sh
+++ .github/workflows/archlinux.sh
@@ -11,18 +11,19 @@ infoend() {
 # Required packages on Archlinux
 requires=(
 	ccache # Use ccache to speed up build
+	clang  # Build with clang on Archlinux
 	meson  # Used for meson build
 )
 
+# https://gitlab.archlinux.org/archlinux/packaging/packages/mate-polkit
 requires+=(
+	accountsservice
 	autoconf-archive
 	gcc
 	git
-	gtk3
-	libappindicator-gtk3
+	gobject-introspection
 	make
 	mate-common
-	polkit
 	which
 )
 
